d982fa8dd938e463f71de79d7fe465ae7a3cac4f
Jinyuan Li <113409361+JinYuanLi0012@users.noreply.github.com>
2023-11-26 01:00:02 +0800

    Add files via upload

---
+++ adaseq/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+# trigger register mechanism
+from adaseq import data, exporters, metrics, models, ms_patch, pipelines, training
+from adaseq.utils.logging import prepare_global_logging
+from adaseq.version import __version__
+
+prepare_global_logging()
